f2b44d0a3e9a905be7da764ffcd02707975b6ce5
Lorden13 <trankhoa1333@gmail.com>
2026-06-10 10:03:19 +0700

    Fix duplicate MembershipCode generation – add millisecond precision

--- backend/Helpers/MembershipCodeGenerator.cs
+++ backend/Helpers/MembershipCodeGenerator.cs
@@ -4,9 +4,10 @@ namespace Auto_Wash.Helpers
 {
     public static class MembershipCodeGenerator
     {
+        // MEM + yyyyMMddHHmmssfff (15 ký tự) -> luôn duy nhất kể cả khi nhiều yêu cầu trong cùng giây
         public static string Generate()
         {
-            return "MEM" + DateTime.Now.ToString("yyMMddHHmmss");
+            return "MEM" + DateTime.UtcNow.ToString("yyMMddHHmmssfff");
         }
     }
 }
